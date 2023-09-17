daeaf9cb195014e47e76f8cb419c2904a8ada7eb
Osama Ahmed <osama.oahz@gmail.com>
2023-09-17 14:05:22 +0300

    Edit

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -30,6 +30,6 @@ export default async function PostDetails({ params }: { params: any }) {
   );
 }
 
-// export async function generateStaticParams() {
-//   return [];
-// }
+export async function generateStaticParams() {
+  return [];
+}
